036fde553c4b6f4dfc7e39311978b3ac5ef66e68
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-11-19 18:10:23 +0200

    Add MockHintDAO

---
+++ Lukuvinkkikirjasto/src/test/java/ohtu/lukuvinkkikirjasto/dao/MockHintDAO.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.lukuvinkkikirjasto.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import ohtu.lukuvinkkikirjasto.hint.Hint;
+import ohtu.lukuvinkkikirjasto.hint.HintClass;
+
+/**
+ *
+ * @author jaakko
+ */
+public class MockHintDAO implements HintDAO {
+    private HashMap<Integer, HintClass> map = new HashMap<>();
+    private int idCounter = 0;
+    
+    @Override
+    public void delete(int id) throws Exception {
+        map.remove(id);
+    }
+
+    @Override
+    public List<HintClass> findAll() throws Exception {
+        return new ArrayList<>(map.values());
+    }
+
+    @Override
+    public HintClass findOne(int id) throws Exception {
+        return map.get(id);
+    }
+
+    @Override
+    public int insert(HintClass object) throws Exception {
+        int id = idCounter++;
+        map.put(id, object);
+        return id;
+    }
+    
+}
