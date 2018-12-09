a17cd4d9cb58e68bc2653a8dc720fb39a0ed86cc
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-12-09 17:51:02 +0200

    Create action to add hints by ISBN

--- Lukuvinkkikirjasto/src/main/java/ohtu/lukuvinkkikirjasto/Main.java
+++ Lukuvinkkikirjasto/src/main/java/ohtu/lukuvinkkikirjasto/Main.java
@@ -3,6 +3,7 @@ package ohtu.lukuvinkkikirjasto;
 import ohtu.lukuvinkkikirjasto.IO.CommandLineIO;
 import ohtu.lukuvinkkikirjasto.IO.IO;
 import ohtu.lukuvinkkikirjasto.UI.App;
+import ohtu.lukuvinkkikirjasto.actions.AddByISBN;
 import ohtu.lukuvinkkikirjasto.dao.HintDAO;
 import ohtu.lukuvinkkikirjasto.dao.MakerDAO;
 import ohtu.lukuvinkkikirjasto.dao.MakerHintAssociationTable;
@@ -21,6 +22,10 @@ import ohtu.lukuvinkkikirjasto.dao.SQLTagDAO;
 import ohtu.lukuvinkkikirjasto.dao.SQLTagHintAssociationTable;
 import ohtu.lukuvinkkikirjasto.dao.TagDAO;
 import ohtu.lukuvinkkikirjasto.dao.TagHintAssociationTable;
+import ohtu.lukuvinkkikirjasto.isbn.ISBNFetcher;
+import ohtu.lukuvinkkikirjasto.isbn.ISBNValidator;
+import ohtu.lukuvinkkikirjasto.isbn.OpenLibraryISBNFetcher;
+import okhttp3.OkHttpClient;
 
 public class Main {
 
@@ -29,6 +34,9 @@ public class Main {
      */
 
     public static void main(String[] args) throws Exception {
+        OkHttpClient httpClient = new OkHttpClient();
+        ISBNFetcher isbnFetcher = new OpenLibraryISBNFetcher(httpClient);
+        
         Database database = new SQLiteDatabase(System.getProperty("user.dir")+"/lukuvinkkikirjasto.db");
         HintDAO hdao = new SQLHintDAO(database);
         TagDAO tdao=new SQLTagDAO(database);
@@ -43,7 +51,8 @@ public class Main {
                 new SearchByTag(hdao, tdao, tagAssociation), 
                 new ShowHint(hdao, tdao, mdao,tagAssociation, makerAssociation),
                 new DeleteHint(hdao), 
-                new ModifyHint(hdao, tdao, tagAssociation));
+                new ModifyHint(hdao, tdao, tagAssociation),
+                new AddByISBN(hdao, tdao, mdao, tagAssociation, makerAssociation, isbnFetcher));
         app.start();
 
         app.join();
